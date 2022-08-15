596c989dc3d4ffce41950ff9c9ac6c3155672350
边宇阳 <1391641952@qq.com>
2022-08-15 17:47:25 +0800

    归档

--- src/views/pigeonhole/Index.vue
+++ src/views/pigeonhole/Index.vue
@@ -40,37 +40,79 @@
 			</el-col>
 
 		</el-row>
-		<!-- 检索结果 -->
-		<el-row :gutter="20" class="goodsindex-list">
-			<el-col :span="24">
-				<el-table :data="tableData" size='small' style="width: 100%">
-					<el-table-column type="index" label="序" width="50">
-					</el-table-column>
-					<el-table-column prop="date" label="产品名称" width="180">
-					</el-table-column>
-					<el-table-column prop="type" label="产品图片" width="120">
-					</el-table-column>
-					<el-table-column prop="content" label="原件" width="80">
-					</el-table-column>
-					<el-table-column prop="name" label="现价" width="80">
-					</el-table-column>
-					<el-table-column prop="name" label="总量" width="80">
-					</el-table-column>
-					<el-table-column prop="name" label="库存" width="80">
-					</el-table-column>
-					<el-table-column prop="name" label="创建者" width="140">
-					</el-table-column>
-					<el-table-column prop="name" label="创建时间" width="180">
-					</el-table-column>
-					<el-table-column prop="name" label="修该者" width="140">
-					</el-table-column>
-					<el-table-column prop="name" label="最后修改时间" width="180">
-					</el-table-column>
-					<el-table-column prop="address" label="操作">
-					</el-table-column>
-				</el-table>
-			</el-col>
-		</el-row>
+		        <!-- 检索结果 -->
+        <el-row :gutter="20">
+            <el-col :span="24">
+                <el-table
+                    :data="tableData"
+                    size='small'
+                    style="width: 100%">
+                    <el-table-column
+                    type="index"
+                    label="序"
+                    width="50">
+                    </el-table-column>
+                    <el-table-column
+                    prop="file_name"
+                    label="名称"
+                    width="150">
+                    </el-table-column>
+                    <el-table-column
+                    prop="task_id"
+                    label="任务号"
+                    width="70">
+                    </el-table-column>
+                    <el-table-column
+                    prop="file_path"
+                    label="文件路径"
+                    width="400">
+                    </el-table-column>
+                    <el-table-column
+                    prop="create_time"
+                    label="创建时间"
+                    width="140">
+                    </el-table-column>
+                    <el-table-column
+                    prop="address"
+                    label="操作">
+                    <template slot-scope="scope">
+                        <el-button @click="sendRequest(scope.row.task_id, scope.row.file_path)">发送请求</el-button>
+                        <el-button @click="checkStatus(scope.row.task_id)">查看状态</el-button>
+                        <el-button @click="checkResult(scope.row.task_id)">查看结果</el-button>
+                    </template>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+        </el-row>
+        <!--结果展示-->
+         <el-dialog :visible.sync="resultShow">
+            <el-row :gutter="20" class="result-list">
+            <el-col :span="24">
+                <el-table
+                    :data="resultData"
+                    size='small'
+                    style="width: 100%">
+                    <el-table-column
+                    type="index"
+                    label="序"
+                    width="50">
+                    </el-table-column>
+                    <el-table-column
+                    prop="type"
+                    label="状态"
+                    width="100">
+                    </el-table-column>
+                    <el-table-column
+                    prop="content"
+                    label="正文"
+                    width="550"
+                    show-overflow-tooltip="true">
+                    </el-table-column>
+                    
+                </el-table>
+            </el-col>
+            </el-row>
+        </el-dialog>
 		<!-- 分页 -->
 		<el-row :gutter="20" class="goodsindex-list">
 			<el-col :span="24" class="goodsindex-page-box">
@@ -97,13 +139,21 @@
 					page: 1,
 					pageSize: 10
 				},
-				tableData: [],
-				queryParams: {
-					task_id: null,
-					batch_size: null,
-					batch_num: null
-				},
+				tableData: [
+            ],
+            resultData:[
+            ],
+				 resultShowParams:{    
+                    task_id:null,  
+                    batch_size:null,    
+                    batch_num:null
+                },
+            sendRequestParams:{
+                task_id:null,
+                file_path:null,
+            },
 				keyCodeShow: false,
+				resultShow:false,
 				keyCodeForm: {
 					name: '',
 					remarks: ''
@@ -126,13 +176,44 @@
 			}
 		},
 		created() {
-			this.getAllList(),
-				handleSizeChange()
+			this.getTask();
 		},
 		methods: {
-			handleSizeChange() {
-				alert("my")
-			},
+			// 检查归档状态
+        checkStatus(task_id){
+            axios.get("http://10.160.181.146:5000/classification/status",{
+                params:{
+                    task_id:task_id
+                }
+            }).then(result=>{
+                alert(result.data.message);
+                this.tableData = result.data;
+            }
+            )
+        },
+        // 发送归档请求
+        sendRequest(task_id, file_path){
+            const params = {...this.sendRequestParams};
+            params.task_id = task_id;
+            params.file_path = file_path;
+            axios.post("http://10.160.181.146:5000/classification",params).then(result=>{
+                alert(result.data.message);
+            }
+            )
+        },
+        // 查看归档结果
+        checkResult(task_id){
+            this.resultShow = true;
+            const params = {...this.resulrShowParams};
+            params.task_id = task_id;
+            params.batch_size = "20";
+            params.batch_num = "1";
+            axios.post("http://10.160.181.146:5000/classification/result",params).then(result=>{
+                console.log(result.data.message);
+                this.resultData = result.data.data;
+            }
+            )
+        },
 			submitUpload() {
 				this.$refs.upload.submit();
 			},
@@ -143,34 +224,41 @@
 				console.log(file);
 			},
 
-			getAllList() {
-				const params = {
-					...this.queryParams
-				};
-				params.task_id = 100;
-				params.batch_size = "1";
-				params.batch_num = "5";
-				axios.post("http://10.160.181.146:5000/classification/result", params).then(result => {
-					console.log(result.data.message);
-					this.tableData = result.data.data;
-				})
-				// axios.get("http://10.160.181.146:5000/classification/status",qs.stringify({
-				//     task_id:"100",
-				// })).then(result=>{
-				//     alert(result.data);
-				//     this.tableData = result.data;
-				// }
-				// )
-				// axios.post("http://10.160.181.146:5000/classification",qs.stringify({
-				//     file_path:"/home/app/test_data/test_data_10.zip",
-				//     task_id:"103",
-				// })).then(result=>{
-				//     alert(result.message);
-				// }
-				// )
+			 // 获取任务
+        getTask(){
+            axios.get("http://10.160.181.146:5000/get_all",{
+                params:{
+                    task_type:"归档"
+                }
+            }).then(result =>{
+                this.tableData = result.data.data;
+            })
+            // const params = {...this.queryParams};
+            // params.task_id = 100;
+            // params.batch_size = "1";
+            // params.batch_num = "5";
+            // axios.post("http://10.160.181.146:5000/classification/result",params).then(result=>{
+            //     console.log(result.data.message);
+            //     this.tableData = result.data.data;
+            // }
+            // )
+            // axios.get("http://10.160.181.146:5000/classification/status",qs.stringify({
+            //     task_id:"100",
+            // })).then(result=>{
+            //     alert(result.data.message);
+            //     this.tableData = result.data;
+            // }
+            // )
+            // axios.post("http://10.160.181.146:5000/classification",qs.stringify({
+            //     file_path:"/home/app/test_data/test_data_10.zip",
+            //     task_id:"103",
+            // })).then(result=>{
+            //     alert(result.message);
+            // }
+            // )
 
-			}
-		}
+        }
+    }
 	}
 </script>
 
@@ -230,4 +318,10 @@
 	.upload-demo {
 		float: right;
 	}
+</style>
+<style>
+.el-tooltip__popper{
+    font-size: 10px;
+    max-width: 70%;
+}
 </style>
